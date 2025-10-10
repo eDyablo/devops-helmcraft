71e1ed47870b8c7b34fb6c8f0890143bdb5f120e
Ed <eyablonsky@gmail.com>
2025-10-10 05:16:46 +0300

    update terminal entrypoint

--- terminal/entrypoint.sh
+++ terminal/entrypoint.sh
@@ -3,8 +3,13 @@ set -e
 mkdir -p ~/.aws && cp -r /opt/.aws ~/
 
 mkdir -p ~/.kube && cp /opt/.kube/config ~/.kube/config
-echo $KUBE_CLUSTER_SERVER_MAP | tr -s ';' '\n' | while read KUBE_CLUSTER KUBE_CLUSTER_SERVER; do
-  ${KUBE_CLUSTER:+kubectl config set-cluster ${KUBE_CLUSTER} --server=${KUBE_CLUSTER_SERVER}}
+
+echo $KUBE_CLUSTER_SERVER_MAP | tr -s ',' '\n' |
+while read KUBE_CLUSTER_SERVER_MAPPING; do
+  echo "$KUBE_CLUSTER_SERVER_MAPPING" | tr -s '=' ' ' | (
+    read KUBE_CLUSTER KUBE_CLUSTER_SERVER
+    ${KUBE_CLUSTER:+kubectl config set-cluster ${KUBE_CLUSTER} --insecure-skip-tls-verify=true --server=${KUBE_CLUSTER_SERVER}}
+  )
 done
 
 PS1='${AWS_PROFILE:+"${AWS_PROFILE} "}$(kubectl config current-context) \w $ ' bash $@
